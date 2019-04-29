fb3884b64859373444b57778cb31ec9524923587
unknown <srietz@scu.edu>
2019-04-28 21:33:34 -0700

    first commit

---
+++ Assets/Scripts/TeleportTile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTile : MonoBehaviour {
+
+    public GameObject player;
+
+	public void Teleport_Player()
+    {
+        player.transform.position = new Vector3(transform.position.x, 2.6f, transform.position.z);
+    }
+}
